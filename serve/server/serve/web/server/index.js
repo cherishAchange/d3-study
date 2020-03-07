e1480e3e532938f73347176874ccd1892033b5f6
ma.ge <18845142535@163.com>
2020-03-08 01:35:58 +0800

    添加备案号,配置websocket代理

--- serve/server/serve/web/server/index.js
+++ serve/server/serve/web/server/index.js
@@ -3,16 +3,17 @@ const app = new Koa();
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
 
+const nsp = io.of('/chat');
 let roomName = `${Math.random()}`;
-io.on('connection', (socket) => {
+nsp.on('connection', (socket) => {
     console.log(`[房间名称：${roomName}]`);
     socket.join(roomName);
 
-    io.to(roomName).emit('people-join', { message: 'One people join us' });
+    nsp.to(roomName).emit('people-join', { message: 'One people join us' });
 
     socket.on('one-request', function (data) {
         console.log(data);
-        io.to(roomName).emit('response-all', { message: data.message });
+        nsp.to(roomName).emit('response-all', { message: data.message });
     });
 });
 
